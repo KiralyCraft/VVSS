d7fb5a26a87fed951abdddefaea31f410acf3c0b
KiralyCraft <alexucu@gmail.com>
2019-05-03 19:45:53 +0300

    Fixed files PLEASE WORK GOD DAMN

--- src/main/java/agenda/model/repository/classes/RepositoryUserFile.java
+++ src/main/java/agenda/model/repository/classes/RepositoryUserFile.java
@@ -15,7 +15,7 @@ import agenda.model.repository.interfaces.RepositoryUser;
 public class RepositoryUserFile implements RepositoryUser{
 
 	private List<User> users;
-	private static final String filename = "files"+File.separator+"FFFFusers.txt";
+	private static final String filename = "files"+File.separator+"users.txt";
 	
 	public RepositoryUserFile() throws Exception 
 	{
